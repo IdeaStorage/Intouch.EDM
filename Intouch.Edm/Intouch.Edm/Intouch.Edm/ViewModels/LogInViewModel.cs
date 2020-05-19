d3b0ea098c476b53fdd266ea46acdd5dd3db796d
Serden Bulut <serdenbulut@gmail.com>
2020-05-19 16:58:40 +0300

    Loading Bar Added to LoginScreen

--- Intouch.Edm/Intouch.Edm/Intouch.Edm/ViewModels/LogInViewModel.cs
+++ Intouch.Edm/Intouch.Edm/Intouch.Edm/ViewModels/LogInViewModel.cs
@@ -12,11 +12,13 @@ namespace Intouch.Edm.ViewModels
         {
         }
 
-        ICommand _signInCommand;
+        private ICommand _signInCommand;
+
         public ICommand SignInCommand => _signInCommand
                 ?? (_signInCommand = new Command(async () => await ExecuteSignInCommand()));
 
         private string _username;
+
         public string Username
         {
             get
@@ -30,6 +32,7 @@ namespace Intouch.Edm.ViewModels
         }
 
         private string _password;
+
         public string Password
         {
             get
@@ -42,35 +45,36 @@ namespace Intouch.Edm.ViewModels
             }
         }
 
-        async System.Threading.Tasks.Task ExecuteSignInCommand()
+        private async System.Threading.Tasks.Task ExecuteSignInCommand()
         {
+            IsBusy = true;
             User user = new User
             {
                 UserNameOrEmailAddress = Username != null ? Username.Trim() : "",
                 Password = Password != null ? Password.Trim() : ""
             };
 
-
             if (string.IsNullOrWhiteSpace(user.UserNameOrEmailAddress) || string.IsNullOrWhiteSpace(user.Password))
             {
+                IsBusy = false;
                 await Application.Current.MainPage.DisplayAlert("UYARI", "Kullanıcı Adı ve Şifre girmelisiniz!", "TAMAM");
                 return;
             }
 
             var response = await DataService.GetAuthTokenAsync(user);
 
-
             Helpers.Settings.Username = user.UserNameOrEmailAddress.Trim();
             Helpers.Settings.Password = user.Password.Trim();
 
             if (!response.Success)
             {
+                IsBusy = false;
                 await Application.Current.MainPage.DisplayAlert("UYARI", "Kullanıcı Adı veya Şifrenizi yanlış girdiniz!", "TAMAM");
             }
             else
             {
                 Helpers.Settings.SetTokenInformation(response);
-                // set 
+                // set
                 if (!string.IsNullOrEmpty(Helpers.Settings.FirebaseNotification))
                 {
                     await DataService.SaveFirebaseTokenAsync(
@@ -81,12 +85,17 @@ namespace Intouch.Edm.ViewModels
                         });
                 }
                 var responsePermission = await DataService.GetPermissionsAsync(Convert.ToInt32(Helpers.Settings.UserId));
-                Helpers.Settings.SetPermission(responsePermission);
+                if (responsePermission.success)
+                {
+                    Helpers.Settings.SetPermission(responsePermission);
+                    IsBusy = false;
+                }
 
                 await Application.Current.MainPage.Navigation.PushAsync(new MainPage());
             }
         }
     }
+
     public class UserMobileAppTokenInput
     {
         public long UserId { get; set; }
