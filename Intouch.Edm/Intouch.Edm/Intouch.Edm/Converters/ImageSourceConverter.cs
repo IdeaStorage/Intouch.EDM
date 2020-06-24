e4c2edfb171f337378e85670a4c416cd8a1a48a5
Serden Bulut <serdenbulut@gmail.com>
2020-06-24 23:32:46 +0300

    İos fotoğraf düzenlemesi yapıldı.

---
+++ Intouch.Edm/Intouch.Edm/Intouch.Edm/Converters/ImageSourceConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using Xamarin.Forms;
+
+namespace Intouch.Edm.Converters
+{
+    public class ImageSourceConverter : IValueConverter
+    {
+        private static WebClient Client = new WebClient();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+            var byteArray = Client.DownloadData(value.ToString());
+            return ImageSource.FromStream(() => new MemoryStream(byteArray));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
