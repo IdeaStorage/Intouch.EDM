1b56ab74f479e477f8c9bf336efdc273d6dad7e1
Serden Bulut <serdenbulut@gmail.com>
2020-07-27 01:07:26 +0300

    Android alt menu yazıları hep görünecek şekilde düzenleme yapıldı.

--- Intouch.Edm/Intouch.Edm/Intouch.Edm.Android/Renderers/MyTabbedPageRenderer.cs
+++ Intouch.Edm/Intouch.Edm/Intouch.Edm.Android/Renderers/MyTabbedPageRenderer.cs
@@ -1,4 +1,5 @@
-﻿using Android.Support.Design.Widget;
+﻿using Android.Support.Design.Internal;
+using Android.Support.Design.Widget;
 using Android.Views;
 using Intouch.Edm.Droid.Renderers.TabbedDemo.Droid;
 using Intouch.Edm.Views;
@@ -41,6 +42,56 @@ namespace Intouch.Edm.Droid.Renderers
                         }
                     }
                 }
+
+                if (ViewGroup != null && ViewGroup.ChildCount > 0)
+                {
+                    BottomNavigationMenuView bottomNavigationMenuView = FindChildOfType<BottomNavigationMenuView>(ViewGroup);
+
+                    if (bottomNavigationMenuView != null)
+                    {
+                        var shiftMode = bottomNavigationMenuView.Class.GetDeclaredField("mShiftingMode");
+
+                        shiftMode.Accessible = true;
+                        shiftMode.SetBoolean(bottomNavigationMenuView, false);
+                        shiftMode.Accessible = false;
+                        shiftMode.Dispose();
+
+                        for (var i = 0; i < bottomNavigationMenuView.ChildCount; i++)
+                        {
+                            BottomNavigationItemView item = bottomNavigationMenuView.GetChildAt(i) as BottomNavigationItemView;
+                            if (item == null) continue;
+
+                            item.SetShiftingMode(false);
+                            item.SetChecked(item.ItemData.IsChecked);
+                        }
+
+                        if (bottomNavigationMenuView.ChildCount > 0)
+                        {
+                            bottomNavigationMenuView.UpdateMenuView();
+                        }
+                    }
+                }
+            }
+
+            private T FindChildOfType<T>(ViewGroup viewGroup) where T : Android.Views.View
+            {
+                if (viewGroup == null || viewGroup.ChildCount == 0) return null;
+
+                for (var i = 0; i < viewGroup.ChildCount; i++)
+                {
+                    var child = viewGroup.GetChildAt(i);
+
+                    T typedChild = child as T;
+                    if (typedChild != null) return typedChild;
+
+                    if (!(child is ViewGroup)) continue;
+
+                    var result = FindChildOfType<T>(child as ViewGroup);
+
+                    if (result != null) return result;
+                }
+
+                return null;
             }
         }
     }
