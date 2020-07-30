9a6ad7fbdc74d85419e753268271768a5e73fd52
Serden Bulut <serdenbulut@gmail.com>
2020-07-30 03:25:16 +0300

    Label shadow eklendi

---
+++ Intouch.Edm/Intouch.Edm/Intouch.Edm.iOS/Effects/LabelShadowEffect.cs
@@ -0,0 +1,38 @@
+﻿using CoreGraphics;
+using Intouch.Edm.Controls;
+using Intouch.Edm.iOS.Effects;
+using System;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportEffect(typeof(LabelShadowEffect), "LabelShadowEffect")]
+
+namespace Intouch.Edm.iOS.Effects
+{
+    public class LabelShadowEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            try
+            {
+                var effect = (ShadowEffect)Element.Effects.FirstOrDefault(e => e is ShadowEffect);
+                if (effect != null)
+                {
+                    Control.Layer.ShadowRadius = effect.Radius;
+                    Control.Layer.ShadowColor = effect.Color.ToCGColor();
+                    Control.Layer.ShadowOffset = new CGSize(effect.DistanceX, effect.DistanceY);
+                    Control.Layer.ShadowOpacity = 1.0f;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
---
+++ Intouch.Edm/Intouch.Edm/Intouch.Edm/Controls/ShadowEffect.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+
+namespace Intouch.Edm.Controls
+{
+    public class ShadowEffect : RoutingEffect
+    {
+        public float Radius { get; set; }
+
+        public Color Color { get; set; }
+
+        public float DistanceX { get; set; }
+
+        public float DistanceY { get; set; }
+
+        public ShadowEffect() : base("Xamarin.LabelShadowEffect")
+        {
+        }
+    }
+}
