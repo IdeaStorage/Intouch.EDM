829f828c6a7fbdbfe4593f865128f20683d59b21
Serden Bulut <serdenbulut@gmail.com>
2020-08-01 00:14:19 +0300

    Combobox ios düzenlemesi yapıldı

--- Intouch.Edm/Intouch.Edm/Intouch.Edm.Android/Renderers/BorderlessPickerRenderer.cs
+++ Intouch.Edm/Intouch.Edm/Intouch.Edm.Android/Renderers/BorderlessPickerRenderer.cs
@@ -1,35 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Contexts;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.Graphics;
+﻿using Android.Graphics;
 using Android.Graphics.Drawables;
 using Android.Graphics.Drawables.Shapes;
-using Android.OS;
-using Android.Runtime;
 using Android.Views;
-using Android.Widget;
 using Intouch.Edm.Controls;
 using Intouch.Edm.Droid;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
-using Xamarin.Forms.Platform.Android.AppCompat;
 
 [assembly: ExportRenderer(typeof(BorderlessPicker), typeof(BorderlessPickerRenderer))]
+
 namespace Intouch.Edm.Droid
 {
-
     public class BorderlessPickerRenderer : Xamarin.Forms.Platform.Android.AppCompat.PickerRenderer
     {
         public BorderlessPickerRenderer(Android.Content.Context context) : base(context)
         {
             AutoPackage = false;
         }
-        BorderlessPicker element;
+
+        private BorderlessPicker element;
+
         protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
         {
             base.OnElementChanged(e);
@@ -40,7 +30,7 @@ namespace Intouch.Edm.Droid
             {
                 Control.Background = AddBorderToPicker();
                 Control.Gravity = GravityFlags.CenterHorizontal;
-               // Control.SetShadowLayer(10.0f, 0.0f, 2.0f, Android.Graphics.Color.Black);
+                Control.SetTextColor(Android.Graphics.Color.Black);
             }
         }
 
@@ -56,19 +46,18 @@ namespace Intouch.Edm.Droid
             outerR[6] = 45;
             outerR[7] = 45;
 
-            ShapeDrawable pickerBorder = new ShapeDrawable(new RoundRectShape(outerR,null, null));
-       
+            ShapeDrawable pickerBorder = new ShapeDrawable(new RoundRectShape(outerR, null, null));
+
             pickerBorder.Paint.Color = Android.Graphics.Color.LightGray;
-             
+
             pickerBorder.Paint.SetStyle(Paint.Style.FillAndStroke);
             pickerBorder.SetPadding(5, 5, 5, 5);
-         
+
             Drawable[] layers = { pickerBorder };
             LayerDrawable layerDrawable = new LayerDrawable(layers);
             layerDrawable.SetLayerInset(0, 0, 0, 0, 0);
-            
+
             return layerDrawable;
         }
     }
-
 }
---
+++ Intouch.Edm/Intouch.Edm/Intouch.Edm.iOS/Renderers/BorderlessPickerRenderer.cs
@@ -0,0 +1,27 @@
+﻿using Intouch.Edm.Controls;
+using Intouch.Edm.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(BorderlessPicker), typeof(BorderlessPickerRenderer))]
+
+namespace Intouch.Edm.iOS
+{
+    public class BorderlessPickerRenderer : PickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.TextAlignment = UITextAlignment.Center;
+                Control.BackgroundColor = UIColor.LightGray;
+                Control.TextColor = UIColor.Black;
+                Control.Layer.CornerRadius = 15;
+                Control.Layer.MasksToBounds = true;
+            }
+        }
+    }
+}
