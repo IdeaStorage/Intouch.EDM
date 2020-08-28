cca7e8afcb6083abe41344a4f6a37d1825705831
Serden Bulut <serdenbulut@gmail.com>
2020-08-29 01:54:11 +0300

    Kullanıcı update ve senaryo onay süreçlerine loading bar eklendi.

--- Intouch.Edm/Intouch.Edm/Intouch.Edm/ViewModels/ScenarioApproveViewModel.cs
+++ Intouch.Edm/Intouch.Edm/Intouch.Edm/ViewModels/ScenarioApproveViewModel.cs
@@ -49,17 +49,22 @@ namespace Intouch.Edm.ViewModels
 
         private async System.Threading.Tasks.Task ApproveClicked()
         {
+            IsBusy = true;
             ApproveScenarioDto approveScenario = new ApproveScenarioDto { id = this.scenario.Id };
 
             var result = await DataService.ApproveScenario(approveScenario);
             string returnMessage = result.result.message;
             if (result.result.approveResult)
             {
+                IsBusy = false;
                 await Application.Current.MainPage.DisplayAlert("ONAYLANDI", returnMessage, "TAMAM");
                 await Application.Current.MainPage.Navigation.PushAsync(new MainPage());
             }
             else
+            {
+                IsBusy = false;
                 await Application.Current.MainPage.DisplayAlert("Başarısız", returnMessage, "TAMAM");
+            }
         }
 
         private ICommand _rejectCommand;
@@ -69,16 +74,21 @@ namespace Intouch.Edm.ViewModels
 
         private async System.Threading.Tasks.Task RejectClicked()
         {
+            IsBusy = true;
             ApproveScenarioDto approveScenario = new ApproveScenarioDto { id = this.Scenario.Id };
 
             var result = await DataService.RejectScenario(approveScenario);
             if (result)
             {
+                IsBusy = false;
                 await Application.Current.MainPage.DisplayAlert("REDDEDİLDİ", "Kayıt Reddilmiştir.", "TAMAM");
                 await Application.Current.MainPage.Navigation.PushAsync(new MainPage());
             }
             else
+            {
+                IsBusy = false;
                 await Application.Current.MainPage.DisplayAlert("Başarısız", "Kayıt Reddedilememiştir.", "TAMAM");
+            }
         }
     }
 }
